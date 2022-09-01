d4957b05a15745d5abffd72de2c74cfbf876624d
AndreyAnisimovTerricon <atriummebel79@yandex.ru>
2022-09-01 23:27:37 +0300

    сумма цифр числа

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+int sum = 0;
+while (n>0)
+{sum = sum + n % 10;
+n /= 10;}
+int result = sum;
+Console.WriteLine ( result);
